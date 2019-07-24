32841ca7c7306c11eefe740f8d9043b21f39eb74
Brett E Stockmeier <stockmeierb@gmail.com>
2019-07-23 23:18:52 -0500

    reusable component: header

---
+++ src/components/common/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Header = props => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}> {props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: "#f8f8f8",
+    justifyContent: "center",
+    aligntItems: "center",
+    height: 70,
+    paddingTop: 25,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: "relative"
+  },
+  textStyle: { fontSize: 20 }
+};
+
+export default Header;
