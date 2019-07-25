ce60e747aa2dd006a02adb6d1c0bd5eb837d0ee3
Brett E Stockmeier <stockmeierb@gmail.com>
2019-07-24 22:46:27 -0500

    firebase initialize app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import { Header } from "./components/common";
 import firebase from "firebase";
 
 class App extends Component {
-  componentWillMount() {}
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBhH-6tUA1mwxBxg4rfBI1BE_J5Bd2laiE",
+      authDomain: "auth-9f3c0.firebaseapp.com",
+      databaseURL: "https://auth-9f3c0.firebaseio.com",
+      projectId: "auth-9f3c0",
+      storageBucket: "",
+      messagingSenderId: "919179840531",
+      appId: "1:919179840531:web:5b3bc080758036d8"
+    });
+  }
   render() {
     return (
       <View>
