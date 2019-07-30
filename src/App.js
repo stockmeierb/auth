74ee879f6e82b8c3b18bb4ac02a7a5b96d37c983
Brett E Stockmeier <stockmeierb@gmail.com>
2019-07-30 14:43:12 -0500

    app renders different content depending on if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import firebase from "firebase";
-import { Header } from "./components/common";
+import { Header, Button } from "./components/common";
 import LoginForm from "./components/LoginForm";
 
 class App extends Component {
@@ -25,11 +25,18 @@ class App extends Component {
       }
     });
   }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return <Button>Log out</Button>;
+    }
+    return <LoginForm />;
+  }
   render() {
     return (
       <View>
         <Header headerText="Authentication!!!" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
